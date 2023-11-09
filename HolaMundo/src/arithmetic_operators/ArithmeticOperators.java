0a3045c799a1fcd2ee828bacb62762763aa6a4a1
Juan Castro <juanjosecastrogutierrez@gmail.com>
2023-11-09 17:34:01 -0500

    arithmetic operators in Java :)

---
+++ HolaMundo/src/arithmetic_operators/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package arithmetic_operators;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1, num2, sum, subtraction, multiplication, division;
+		num1 = 4;
+		num2 = 2;
+		
+		sum = num1 + num2;
+		subtraction = num1 - num2;
+		multiplication = num1 * num2;
+		division = num1 / num2;
+		
+		System.out.println("the sum result is: " + sum);
+		System.out.println("the subtraction result is: " + subtraction);
+		System.out.println("the multiplication result is: " + multiplication);
+		System.out.println("the division result is: " + division);
+	}
+
+}
