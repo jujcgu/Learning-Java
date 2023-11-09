dcfd1755c52e41d501dcbd56452a761bdf24d935
Juan Castro <juanjosecastrogutierrez@gmail.com>
2023-11-09 17:08:41 -0500

    data types and variables

---
+++ HolaMundo/src/variables/Variable.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 5;
+		String name = "Juan";
+		char letter = 'J';
+		double temperature = 35.5;
+		boolean minor = false;
+		long id = 1003802090;
+
+	}
+
+}
