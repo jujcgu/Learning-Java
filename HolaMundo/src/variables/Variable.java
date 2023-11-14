4833d48afc7e426227f9a198d6604254ea33d17c
jujcgu <juanjosecastrogutierrez@gmail.com>
2023-11-14 15:15:32 -0500

    ToDo deleted in source code

--- HolaMundo/src/variables/Variable.java
+++ HolaMundo/src/variables/Variable.java
@@ -3,8 +3,7 @@ package variables;
 public class Variable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		int number = 5;
 		String name = "Juan";
 		char letter = 'J';
