22c222151531b76a294a404c648736b9c839ce13
Adam <Adam@DESKTOP-80I0FLK>
2023-11-14 15:10:07 -0500

    adam 12

---
+++ HolaMundo/src/conditional/MultipleConditional.java
@@ -0,0 +1,23 @@
+package conditional;
+
+public class MultipleConditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 1;
+		int num2 = 10;
+
+		if (num2 > num1) {
+			System.out.println("el num2 es mayor al num1");
+		} else {
+			if (num2 == num1) {
+				System.out.println("el num2 es igual al num1");
+			} else {
+				System.out.println("en num2 no es mayor al num1");
+			}
+		}
+
+	}
+
+}
---
+++ HolaMundo/src/conditional/SwitchConditional.java
@@ -0,0 +1,45 @@
+package conditional;
+
+public class SwitchConditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day = 8;
+		String dayName;
+
+		switch (day) {
+		case 1: {
+			dayName = "Sunday";
+		}
+			break;
+		case 2: {
+			dayName = "Monday";
+		}
+			break;
+		case 3: {
+			dayName = "Tuesday";
+		}
+			break;
+		case 4: {
+			dayName = "Wednesday";
+		}
+			break;
+		case 5: {
+			dayName = "Thursday";
+		}
+			break;
+		case 6: {
+			dayName = "Friday";
+		}
+			break;
+		case 7: {
+			dayName = "Saturday";
+		}
+			break;
+		default:
+			dayName = "Invalid day";
+		}
+		System.out.println(dayName);
+	}
+
+}
